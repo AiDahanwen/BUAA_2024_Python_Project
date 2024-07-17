2b1945b4d527648d5f2de44ff3465be861a4a52b
Foolish-Han <2892278592@qq.com>
2024-07-17 12:19:56 +0800

    backend-update-add user system-Foolish-Han

--- README.md
+++ README.md
@@ -10,17 +10,7 @@
 
 ## database.py
 
-#### `get_constant(file_name, constant_name)`
-
-用于从数据库中获得常量的值，参数为**文件名**和**常量名**，返回值均为**字符串类型**。下面的代码可以获得发送方邮箱地址。
-
-```python
-send_by = get_constant(file_name, 'send_by')
-```
-
-把一些参数常量放在数据库，是为了不用修改代码就可以直接改变程序的运行状况。
-
-#### 数据库
+### 数据库
 
 使用阿里云数据库，可通过外网链接访问。
 
@@ -32,9 +22,43 @@ passwd='DCJBzxhy2024'
 database='python_todo'
 ```
 
+### 密码机制
+
+数据库中存储的是经过哈希加密后的密码以及加密所使用的盐。
+
+- 生成密码时，将随机生成盐对输入的密码进行加密，再将二者存储到数据库中
+
+- 检测密码正确与否时，将从数据库中获取盐对输入的密码进行加密，再与数据库中的密码进行比对
+
+### `get_constant(file_name, constant_name)`
+
+用于从数据库中获得常量的值，参数为 **文件名**和 **常量名**，返回值均为 **字符串类型**。下面的代码可以获得发送方邮箱地址。
+
+```python
+send_by = get_constant(file_name, 'send_by')
+```
+
+把一些参数常量放在数据库，是为了不用修改代码就可以直接改变程序的运行状况。
+
+### `add_user(user_name, user_email, user_password)`
+
+用于向数据库中新增用户，参数为 **用户名**、**用户邮箱（需要保证唯一）**和 **用户密码**，返回值为 **True** 或 **False** 代表操作是否成功。
+
+### `delete_user(user_email)`
+
+用于删除数据库中的指定用户，参数为**用户邮箱（需要保证唯一）**，返回值为 **True** 或 **False** 代表操作是否成功。
+
+### `reset_password(user_email, user_password)`
+
+用于重置数据库中指定用户的密码，参数为**用户邮箱（需要保证唯一）**和 **新的用户密码**，返回值为 **True** 或 **False** 代表操作是否成功。
+
+### `is_user_password_correct(user_email, user_password)`
+
+用于检查指定用户的密码是否正确，参数为**用户邮箱（需要保证唯一）**和 **输入的用户密码**，返回值为 **True** 或 **False** 代表密码是否正确。
+
 ## send_email_code.py
 
-#### `send_email_code(send_to)`
+### `send_email_code(send_to)`
 
 用于向指定邮箱发送验证码，参数为**接收方邮箱地址**，发送成功将返回**验证码（字符串类型）**，发送失败将返回**False**。验证码为随机生成的六位大小写字母、数字的组合。下面的代码可以向`2892278592@qq.com` 发送并返回验证码。
 
@@ -51,9 +75,9 @@ mail_host = 'smtp.qq.com'
 send_port = 465
 ```
 
-### daily_sentence.py
+## daily_sentence.py
 
-#### `get_sentence()`
+### `get_sentence()`
 
 用于随机产生每日一句格言，内容涵盖文学、影视、诗词、哲学等方面，返回值为**格言内容（字符串类型）与出处的元组**。下面的代码可以获得一句格言及出处。
 
--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,125 @@
 import pymysql
+import bcrypt
+import re
+
+
+def database_connect():
+    return pymysql.connect(host='rm-cn-g6z3tw7xl00052co.rwlb.rds.aliyuncs.com',
+                           port=3306,
+                           user='buaa_python_2024',
+                           passwd='DCJBzxhy2024',
+                           db='python_todo',
+                           charset='utf8mb4',
+                           binary_prefix=True)
+
+
+def database_read(cmd, args):
+    try:
+        connection = database_connect()
+        cursor = connection.cursor()
+        query = cmd
+        cursor.execute(query, args)
+        result = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return result[0] if result else None
+    except e:
+        return False
+
+
+def database_write(cmd, args):
+    try:
+        connection = database_connect()
+        cursor = connection.cursor()
+        cursor.execute(cmd, args)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return True
+    except e:
+        return False
+
+
+def is_valid_email(email):
+    pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    if re.match(pattern, email):
+        return True
+    else:
+        return False
+
+
+def gen_hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return salt, hashed_password
+
+
+def get_hash_password(password, salt):
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
 
 
 def get_constant(file_name, constant_name):
-    connection = pymysql.connect(host='rm-cn-g6z3tw7xl00052co.rwlb.rds.aliyuncs.com',
-                                 port=3306,
-                                 user='buaa_python_2024',
-                                 passwd='DCJBzxhy2024',
-                                 db='python_todo')
-    cursor = connection.cursor()
-    query = "SELECT constant_value FROM constants WHERE  constant_name = %s AND file_name = %s"
-    cursor.execute(query, (constant_name, file_name))
-    result = cursor.fetchone()
-    cursor.close()
-    connection.close()
-    return result[0] if result else None
+    cmd = """
+        SELECT constant_value
+        FROM constants
+        WHERE  constant_name = %s AND file_name = %s"""
+    args = (constant_name, file_name)
+    return database_read(cmd, args)
+
+
+def add_user(user_name, user_email, user_password):
+    cmd = """
+        INSERT INTO users (user_name, user_email, user_password_hash, hash_salt)
+        VALUES(%s, %s, %s, %s)
+        """
+    salt, user_password_hash = gen_hash_password(user_password)
+    args = (user_name, user_email, user_password_hash, salt)
+    return database_write(cmd, args)
+
+
+def reset_password(user_email, user_password):
+    salt, user_password_hash = gen_hash_password(user_password)
+    cmd = """
+        UPDATE users
+        SET user_password_hash = %s, hash_salt = %s
+        WHERE user_email = %s
+        """
+    args = (user_password_hash, salt, user_email)
+    return database_write(cmd, args)
+
+
+def delete_user(user_email):
+    cmd = """
+    DELETE FROM users
+    WHERE user_email = %s
+    """
+    args = (user_email,)
+    return database_write(cmd, args)
+
+
+def is_user_password_correct(user_email, user_password):
+    cmd = """
+        SELECT hash_salt
+        FROM users
+        WHERE user_email = %s
+    """
+    args = (user_email,)
+    salt = database_read(cmd, args)
+    if not salt:
+        return False
+    user_password_hash = get_hash_password(user_password, salt)
+    cmd = """
+        SELECT user_password_hash
+        FROM users
+        WHERE user_email = %s
+    """
+    args = (user_email,)
+    result = database_read(cmd, args)
+    if result:
+        return user_password_hash == result
+    return False
+
+
+# print(add_user('ass', 'sss', 'sss'))
+# print(reset_password('sss', 'bbb'))
+print(is_user_password_correct('sss', 'bbb'))
