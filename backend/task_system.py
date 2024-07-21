1f5e868dfd84774696544b6c05a316f13a29207e
真不想写代码 <3498314162@qq.com>
2024-07-21 21:34:48 +0800

    backend-refactor-task_system.py-yhma

--- backend/task_system.py
+++ backend/task_system.py
@@ -9,67 +9,54 @@ class TaskStatus(str):
     EXPIRED = 'expired'
 
 
+class TaskVital(int):
+    TRIVIAL = 0
+    NORMAL = 1
+    CRUCIAL = 2
+
+
 class Task:
 
     def __init__(self, user_email, **kwargs):
         self.task_id = kwargs.get('task_id', 0)
         self.user_email = user_email
+        self.daily_task_id = kwargs.get('daily_task_id', 0)
         self.task_status = kwargs.get('task_status', TaskStatus.PENDING)
-        self.task_is_vital = kwargs.get('task_is_vital', 0)
+        self.task_vital = kwargs.get('task_vital', TaskVital.TRIVIAL)
         self.task_title = kwargs.get('task_title', 'some_task')
-        self.task_content = kwargs.get('task_content', None)
-        self.task_description = kwargs.get('task_description')
-        self.task_tag = kwargs.get('task_tag', None)
+        self.task_content = kwargs.get('task_content', '')
+        self.task_tag = kwargs.get('task_tag', '其他')
         self.task_is_daily = kwargs.get('task_is_daily', 0)
-        self.task_start_time = kwargs.get('task_start_time', None)
-        self.task_end_time = kwargs.get('task_end_time', None)
-        self.task_deadline = kwargs.get('task_deadline', None)
-        self.task_repeat_start_time = kwargs.get('task_repeat_start_time', None)
-        self.task_repeat_end_time = kwargs.get('task_repeat_end_time', None)
-        self.task_repeat_start_date = kwargs.get('task_repeat_start_date', None)
-        self.task_repeat_end_date = kwargs.get('task_repeat_end_date', None)
+        self.task_start_time = kwargs.get('task_start_time', datetime.now())
+        self.task_end_time = kwargs.get('task_end_time', datetime.now() + timedelta(days=10))
+        self.task_duration_time = kwargs.get('task_duration_time', timedelta(hours=10))
+        self.task_elapsed_time = kwargs.get('task_elapsed_time', 0)
 
     def __str__(self):
         return f'task_id: {self.task_id}\t' \
                f'user_email: {self.user_email}\t' \
+               f'daily_task_id: {self.daily_task_id}' \
                f'task_status: {self.task_status}\t' \
-               f'task_is_vital: {self.task_is_vital}\t' \
+               f'task_vital: {self.task_vital}\t' \
                f'task_title: {self.task_title}\t' \
                f'task_content: {self.task_content}\t' \
-               f'task_description: {self.task_description}\t' \
                f'task_tag: {self.task_tag}' \
                f'task_is_daily: {self.task_is_daily}' \
                f'task_start_time: {self.task_start_time}\t' \
                f'task_end_time: {self.task_end_time}\t' \
-               f'task_deadline: {self.task_deadline}\t' \
-               f'task_repeat_start_time: {self.task_repeat_start_time}' \
-               f'task_repeat_end_time: {self.task_repeat_end_time}\t' \
-               f'task_repeat_start_date: {self.task_repeat_start_date}\t' \
-               f'task_repeat_end_date: {self.task_repeat_end_date}\t'
+               f'task_duration_time: {self.task_duration_time}' \
+               f'task_elapsed_time: {self.task_elapsed_time}'
 
 
 def add_task(task):
-    if not task.task_is_daily:
-        cmd = """
-        INSERT INTO tasks (user_email, task_status, task_is_vital,
-        task_title, task_content, task_description,task_tag, task_start_time, task_end_time, task_deadline)
-        VALUE (%s, %s, %s ,%s, %s, %s, %s, %s, %s, %s)
+    cmd = """
+        INSERT INTO tasks (user_email, daily_task_id, task_status, task_vital,
+        task_title, task_content, task_tag, task_is_daily, task_start_time, task_end_time, task_duration_time)
+        VALUE (%s, %s, %s ,%s, %s, %s, %s, %s, %s, %s, %s)
         """
-        args = (
-            task.user_email, task.task_status, task.task_is_vital, task.task_title, task.task_content,
-            task.task_description, task.task_tag, task.task_start_time, task.task_end_time, task.task_deadline)
-    else:
-        cmd = """
-        INSERT INTO tasks (user_email, task_status, task_is_vital, task_title, task_content, task_description, 
-        task_tag, task_is_daily, 
-        task_repeat_start_time, task_repeat_end_time, task_repeat_start_date, task_repeat_end_date)
-        VALUE (%s, %s, %s ,%s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """
-        args = (
-            task.user_email, task.task_status, task.task_is_vital, task.task_title, task.task_content,
-            task.task_description, task.task_tag, task.task_is_daily,
-            task.task_repeat_start_time, task.task_repeat_end_time,
-            task.task_repeat_start_date, task.task_repeat_end_date)
+    args = (
+        task.user_email, task.daily_task_id, task.task_status, task.task_vital, task.task_title, task.task_content,
+        task.task_tag, task.task_is_daily, task.task_start_time, task.task_end_time, task.task_duration_time)
     return database_write(cmd, args)
 
 
@@ -89,22 +76,15 @@ def get_task_info(task_id, info_category):
 
 
 def get_task(task_id):
-    get("tasks", "task_id", task_id)
+    return get("tasks", "task_id", task_id)
 
 
-def get_tasks(user_email, condition_cmd='', condition_args=()):
-    cmd = """
-    SELECT t.*
-    FROM tasks t
-    JOIN users u ON t.user_email = u.user_email
-    WHERE u.user_email = %s
-    """ + condition_cmd
-    args = (user_email,) + condition_args
-    data = database_read(cmd, args, False)
-    if not data:
-        return False
-    else:
-        return _get_task_objects(data)
+def _get_task_objects_of_user_with_condition(user_email, condition_cmd='', condition_args=()):
+    return _get_task_objects(join("tasks", "users", "user_email", user_email, condition_cmd, condition_args))
+
+
+def get_task_object_of_user(user_email):
+    return _get_task_objects_of_user_with_condition(user_email)
 
 
 def modify_task(task):
@@ -115,24 +95,24 @@ def modify_task(task):
     WHERE task_id = %s
     """
     args = (
-        task.task_status, task.task_is_vital, task.task_title, task.task_content, task.task_start_time,
+        task.task_status, task.task_vital, task.task_title, task.task_content, task.task_start_time,
         task.task_end_time,
         task.task_deadline, task.task_id)
     return database_write(cmd, args)
 
 
 def get_tasks_date(user_email, date):
-    return get_tasks(user_email, 'AND DATE(t.task_start_time) = %s', (date,))
+    return _get_task_objects_of_user_with_condition(user_email, 'AND DATE(t.task_start_time) = %s', (date,))
 
 
 def get_tasks_date_status(user_email, date, status):
     condition_cmd = """AND DATE(t.task_start_time) = %s AND t.task_status = %s"""
-    return get_tasks(user_email, condition_cmd, (date, status))
+    return _get_task_objects_of_user_with_condition(user_email, condition_cmd, (date, status))
 
 
 def get_tasks_status(user_email, status):
-    condition_cmd = """AND t.task_status = %s"""
-    return get_tasks(user_email, condition_cmd, (status,))
+    condition_cmd = 'AND t.task_status = %s'
+    return _get_task_objects_of_user_with_condition(user_email, condition_cmd, (status,))
 
 
 def get_ordered_tasks_date(user_email, date):
@@ -187,14 +167,9 @@ def _get_task_objects(data):
     result = []
     for line in data:
         result.append(
-            Task(line[1], task_id=line[0], task_status=line[2], task_is_vital=line[3],
-                 task_title=line[4], task_content=line[5], task_description=line[6],
-                 task_tag=line[7], task_is_daily=line[8], task_start_time=line[9],
-                 task_end_time=line[10], task_deadline=line[11], task_repeat_start_time=line[12],
-                 task_repeat_end_time=line[13], task_repeat_start_date=line[14], task_repeat_end_date=line[15]))
+            Task(line[1], task_id=line[0], daily_task_id=[2], task_status=line[3], task_vital=line[4],
+                 task_title=line[5], task_content=line[6], task_tag=line[7], task_is_daily=line[8],
+                 task_start_time=line[9], task_end_time=line[10], task_duration_time=line[11],
+                 task_elapsed_time=line[12]))
     return result
 
-
-print(add_task(Task("123", task_status=TaskStatus.UNDERWAY, task_is_vital=True, task_is_daily=1,
-                    task_repeat_start_date=datetime.today(),
-                    task_repeat_end_date=(datetime.today() + timedelta(10)))))
