4c60418d46f590d70329b94268a9d7fd8b88b9f0
真不想写代码 <3498314162@qq.com>
2024-07-21 15:28:40 +0800

    backend-feat-finish task_record_system.py-yhma

--- backend/task_record_system.py
+++ backend/task_record_system.py
@@ -39,24 +39,8 @@ def get_task_record_info(task_record_id, info_category):
     return get_info("task_record", "task_record_id", task_record_id, info_category)
 
 
-def get_task_record(task_record_id):
-    return get("task_records", "task_record_id", task_record_id)
-
-
-def _get_task_records_of_user_with_condition(user_email, condition_cmd='', condition_args=()):
-    return join("task_records", "users", "user_email", user_email, condition_cmd, condition_args)
-
-
-def get_task_record_objects_of_user(user_email):
-    return get_task_objects(_get_task_records_of_user_with_condition(user_email))
-
-
-def _get_task_records_of_task_with_condition(task_id, condition_cmd='', condition_args=()):
-    return join("task_records", "tasks", "task_id", task_id, condition_cmd, condition_args)
-
-
-def get_task_record_objects_of_task(task_id):
-    return _get_task_record_objects(_get_task_records_of_task_with_condition(task_id))
+def get_task_record_object(task_record_id):
+    return _get_task_record_objects(get("task_records", "task_record_id", task_record_id))
 
 
 def _get_task_record_objects(task_records):
@@ -67,4 +51,34 @@ def _get_task_record_objects(task_records):
     return result
 
 
-print_list(get_task_record_objects_of_task(4))
+def _get_task_record_objects_of_user_with_condition(user_email, condition_cmd='', condition_args=()):
+    return _get_task_record_objects(
+        join("task_records", "users", "user_email", user_email, condition_cmd, condition_args))
+
+
+def get_task_record_objects_of_user(user_email):
+    return _get_task_record_objects_of_user_with_condition(user_email)
+
+
+def get_task_record_objects_of_user_in_time_frame(user_email, task_record_start_time, task_record_end_time):
+    condition_cmd = 'AND task_record_complete_time BETWEEN %s AND %s'
+    condition_args = (task_record_start_time, task_record_end_time)
+    return _get_task_record_objects_of_user_with_condition(user_email, condition_cmd, condition_args)
+
+
+def _get_task_record_objects_of_task_with_condition(task_id, condition_cmd='', condition_args=()):
+    return _get_task_record_objects(
+        join("task_records", "tasks", "task_id", task_id, condition_cmd, condition_args))
+
+
+def get_task_record_objects_of_task(task_id):
+    return _get_task_record_objects_of_task_with_condition(task_id)
+
+
+def get_task_record_objects_of_task_in_time_frame(task_id, task_record_start_time, task_record_end_time):
+    condition_cmd = 'AND task_record_complete_time BETWEEN %s AND %s'
+    condition_args = (task_record_start_time, task_record_end_time)
+    return _get_task_record_objects_of_task_with_condition(task_id, condition_cmd, condition_args)
+
+
+print_list(get_task_record_objects_of_user_in_time_frame("123456", '2024-07-01 00:00:00', '2024-07-21 00:00:00'))
