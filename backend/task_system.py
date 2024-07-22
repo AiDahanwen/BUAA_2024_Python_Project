388cc080f044d323ff6957960fa86e7f98787899
真不想写代码 <3498314162@qq.com>
2024-07-22 21:40:34 +0800

    backend-fix&feat&refactor-debug&update: get_task_schedule_objects()-yhma

--- backend/task_system.py
+++ backend/task_system.py
@@ -1,4 +1,6 @@
 from datetime import (datetime, timedelta, date, time)
+from enum import Enum
+
 from database import *
 
 
@@ -15,10 +17,16 @@ class TaskVital(int):
     CRUCIAL = 2
 
 
-class TaskTimePeriod(str):
-    MORNING = 'morning'
-    AFTERNOON = 'afternoon'
-    EVENING = 'evening'
+class TaskTimePeriod(Enum):
+    MORNING = 0
+    AFTERNOON = 1
+    EVENING = 2
+
+    def __str__(self):
+        return self.name
+
+    def __lt__(self, other):
+        return self.value < other.value
 
 
 class TaskSchedule:
@@ -66,6 +74,7 @@ class Task:
                f'task_elapsed_time: {self.task_elapsed_time}\n'
 
 
+# 增
 def add_task(task):
     cmd = """
         INSERT INTO tasks (user_email, daily_task_id, task_status, task_vital,
@@ -78,6 +87,7 @@ def add_task(task):
     return database_write(cmd, args)
 
 
+# 删
 def delete_task(*task_id):
     for item in task_id:
         if not delete("tasks", "task_id", item):
@@ -85,6 +95,7 @@ def delete_task(*task_id):
     return True
 
 
+# 改
 def reset_task_info(task_id, info_category, task_info):
     return reset_info("task", "task_id", task_id, info_category, task_info)
 
@@ -99,12 +110,24 @@ def add_elapsed_time(task_id, elapsed_time):
     return database_write(cmd, args)
 
 
+# 查
 def get_task_info(task_id, info_category):
     return get_info("task", "task_id", task_id, info_category)
 
 
 def get_task(task_id):
     return get("tasks", "task_id", task_id)
+
+
+def _get_task_objects(data):
+    result = []
+    for line in data:
+        result.append(
+            Task(line[1], task_id=line[0], daily_task_id=line[2], task_status=line[3], task_vital=line[4],
+                 task_title=line[5], task_content=line[6], task_tag=line[7], task_is_daily=line[8],
+                 task_start_time=line[9], task_end_time=line[10], task_duration_time=line[11],
+                 task_elapsed_time=line[12]))
+    return result
 
 
 def _get_task_objects_of_user_with_condition(user_email, condition_cmd='', condition_args=()):
@@ -115,40 +138,43 @@ def get_task_object_of_user(user_email):
     return _get_task_objects_of_user_with_condition(user_email)
 
 
-def modify_task(task):
-    cmd = """
-    UPDATE tasks
-    SET task_status = %s, task_is_vital = %s, task_title = %s, task_content = %s, \
-    task_start_time = %s, task_end_time = %s, task_deadline = %s
-    WHERE task_id = %s
+def get_task_objects_of_user_in_date(user_email, some_date):
+    condition_cmd = """
+    AND DATE(t.task_start_time) <= %s 
+    AND DATE(t.task_end_time) >= %s 
     """
-    args = (
-        task.task_status, task.task_vital, task.task_title, task.task_content, task.task_start_time,
-        task.task_end_time,
-        task.task_deadline, task.task_id)
-    return database_write(cmd, args)
+    return _get_task_objects_of_user_with_condition(user_email, condition_cmd, (some_date,))
 
 
-def get_tasks_date(user_email, date):
-    return _get_task_objects_of_user_with_condition(user_email, 'AND DATE(t.task_start_time) = %s', (date,))
+def get_tasks_of_user_with_status(user_email, status):
+    condition_cmd = 'AND t.task_status = %s'
+    return _get_task_objects_of_user_with_condition(user_email, condition_cmd, (status,))
 
 
-def get_task_objects_of_user_with_status_in_date(user_email, date, status):
+def get_task_objects_of_user_with_status_in_date(user_email, some_date, status):
     condition_cmd = """
     AND DATE(t.task_start_time) <= %s 
     AND DATE(t.task_end_time) >= %s 
     AND t.task_status = %s
     """
-    return _get_task_objects_of_user_with_condition(user_email, condition_cmd, (date, date, status))
+    return _get_task_objects_of_user_with_condition(user_email, condition_cmd, (some_date, some_date, status))
 
 
-def get_tasks_status(user_email, status):
-    condition_cmd = 'AND t.task_status = %s'
-    return _get_task_objects_of_user_with_condition(user_email, condition_cmd, (status,))
+def modify_task(task):
+    cmd = """
+    UPDATE tasks
+    SET task_status = %s, task_vital = %s, task_title = %s, task_content = %s, task_tag = %s, \
+    task_start_time = %s, task_end_time = %s, task_duration_time = %s
+    WHERE task_id = %s
+    """
+    args = (
+        task.task_status, task.task_vital, task.task_title, task.task_content, task.task_tag, task.task_start_time,
+        task.task_end_time, task.task_duration_time, task.task_id)
+    return database_write(cmd, args)
 
 
 def get_ordered_tasks_date(user_email, date):
-    tasks_date = get_tasks_date(user_email, date)
+    tasks_date = get_task_objects_of_user_in_date(user_email, date)
     return sorted(tasks_date, key=lambda x: x.task_start_time)
 
 
@@ -180,7 +206,7 @@ def update_task_status(user_email):
 
 
 def get_task_completed_sum(user_email):
-    tasks = get_tasks_status(user_email, TaskStatus.COMPLETED)
+    tasks = get_tasks_of_user_with_status(user_email, TaskStatus.COMPLETED)
     task_num = len(tasks)
     duration_sum = sum([(_.task_end_time - _.task_start_time).total_seconds() / 3600 for _ in tasks])
     workday_num = len(
@@ -195,26 +221,16 @@ def get_task_completed_date(user_email, date):
     return task_num, duration_sum
 
 
-def _get_task_objects(data):
-    result = []
-    for line in data:
-        result.append(
-            Task(line[1], task_id=line[0], daily_task_id=line[2], task_status=line[3], task_vital=line[4],
-                 task_title=line[5], task_content=line[6], task_tag=line[7], task_is_daily=line[8],
-                 task_start_time=line[9], task_end_time=line[10], task_duration_time=line[11],
-                 task_elapsed_time=line[12]))
-    return result
-
-
 def get_task_schedule_objects(user_email, morning_time, afternoon_time, evening_time):
     # 处理时间
     morning_time = timedelta(hours=morning_time)
     afternoon_time = timedelta(hours=afternoon_time)
     evening_time = timedelta(hours=evening_time)
-    current_date = datetime.today()
+    current_date = date.today()
 
     # 获取任务列表
-    task_objects_list = get_task_objects_of_user_with_status_in_date(user_email, current_date, TaskStatus.UNDERWAY)
+    task_objects_list = get_task_objects_of_user_with_status_in_date(user_email, current_date, TaskStatus.UNDERWAY) + \
+                        get_task_objects_of_user_with_status_in_date(user_email, current_date, TaskStatus.PENDING)
     task_objects_list.sort(key=lambda x: (x.task_end_time, -x.task_vital), reverse=True)
 
     # 规划日程
@@ -240,6 +256,8 @@ def get_task_schedule_objects(user_email, morning_time, afternoon_time, evening_
             task.task_elapsed_time += task_time
             evening_time -= task_time
 
+    # 排序
+    task_schedule_list.sort(key=lambda x: x.task_time_period)
     return task_schedule_list
 
 
@@ -254,8 +272,11 @@ def _get_time(task_time_period, task):
     else:
         time_period_start = datetime.combine(date.today(), time(hour=18))
         time_period_end = datetime.combine(date.today(), time(hour=23, minute=30))
-        
+
     start = max([time_period_start, task.task_start_time])
     end = min([time_period_end, task.task_end_time])
 
-    return min([end - start, task.task_duration_time - task.task_elapsed_time])
+    return max([min([end - start, task.task_duration_time - task.task_elapsed_time]), timedelta(0)])
+
+
+print_list(get_task_schedule_objects("test", 1, 1, 1))
